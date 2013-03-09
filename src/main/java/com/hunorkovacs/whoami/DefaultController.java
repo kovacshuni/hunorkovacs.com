60cf85dcc33f8d2013da7974378e2f00fc28e85c
Hunor Kovács <kovacshuni@yahoo.com>
2013-03-09 23:55:09 +0200

    Static resources are working!!

--- src/main/java/com/hunorkovacs/whoami/DefaultController.java
+++ src/main/java/com/hunorkovacs/whoami/DefaultController.java
@@ -3,14 +3,14 @@ package com.hunorkovacs.whoami;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@RequestMapping("/")
 public class DefaultController {
 
-    @RequestMapping(value = "/**", method = RequestMethod.GET)
-    public RedirectView get() {
+    @RequestMapping(value = "/*", method = RequestMethod.GET)
+    public View get() {
         return new RedirectView("/hard-to-remember.html");
     }
 }
