47e6ab3356002b779ff9b753f3a1cc451dceeffb
Hunor Kovács <Hunor.Kovacs@FT.com>
2013-03-12 15:10:25 +0200

    Extended redirect testing with post, put, delete methods.

--- src/test/java/com/hunorkovacs/whoami/DefaultControllerIT.java
+++ src/test/java/com/hunorkovacs/whoami/DefaultControllerIT.java
@@ -9,11 +9,17 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.Resource;
 
+import static com.sun.corba.se.impl.naming.cosnaming.TransientNameServer.trace;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -33,9 +39,31 @@ public class DefaultControllerIT {
     }
 
     @Test
-    public void testRedirectAnyUrlToFirstPage() throws Exception {
+    public void testBlankGetRedirectsToFirstPage() throws Exception {
+        mockMvc.perform(get("/").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isMovedTemporarily())
+                .andExpect(redirectedUrl("/static/hard-to-remember.html"));
+    }
+
+    @Test
+    public void testAnyUrlGetRedirectsToFirstPage() throws Exception {
         mockMvc.perform(get("/1/a/2/b/3/c/4d/5e/6f/anything").accept(MediaType.TEXT_HTML))
                 .andExpect(status().isMovedTemporarily())
                 .andExpect(redirectedUrl("/static/hard-to-remember.html"));
+    }
+
+    @Test
+    public void testPostNotAllowed() throws Exception {
+        mockMvc.perform(post("/")).andExpect(status().isMethodNotAllowed());
+    }
+
+    @Test
+    public void testPutNotAllowed() throws Exception {
+        mockMvc.perform(put("/")).andExpect(status().isMethodNotAllowed());
+    }
+
+    @Test
+    public void testDeleteNotAllowed() throws Exception {
+        mockMvc.perform(delete("/")).andExpect(status().isMethodNotAllowed());
     }
 }
