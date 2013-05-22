8ae820340acb06019b0d58f36cdec68023237493
Hunor Kovács <kovacshuni@yahoo.com>
2013-05-22 22:14:15 +0300

    Refactored delayed fade function. Cleaner now.

--- src/main/webapp/Who-Am-I-comics/js/fading.js
+++ src/main/webapp/Who-Am-I-comics/js/fading.js
@@ -1,17 +1,10 @@
-var fade = function () {
-    return {
-        delayed: function (id, delay) {
-            setTimeout(function() { fade.init(id) }, delay * 1000);
-        },
-        init: function (id) {
-            fadeIt(id);
-        }
-    }
-}();
-
-function fadeIt(id) {
+function fadeIn(id) {
     var element = document.getElementById(id)
     if (element.className.search("hovered") < 0) {
         element.className = element.className + "-hovered";
     }
+}
+
+function fadeInDelayed(id, delay) {
+    setTimeout(function() { fadeIn(id) }, delay * 1000);
 }
