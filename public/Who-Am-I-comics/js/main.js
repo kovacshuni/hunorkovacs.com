3cb7277d927eabc4c7e0d49ea60d9ac1a1dd4e32
Hunor Kovács <kovacshuni@yahoo.com>
2014-01-17 00:23:07 +0200

    Removed footer from comics pages. Dropped unnecessary functions from main.js, calls from comics pages. Cookie warning, copyright warning.

--- public/Who-Am-I-comics/js/main.js
+++ public/Who-Am-I-comics/js/main.js
@@ -1,6 +1,3 @@
-var COPYRIGHT_COOKIE_NAME = "copyright-acknowledgement";
-var COPYRIGHT_COOKIE_VALUE = "true";
-
 function fadeIn(id) {
     var element = document.getElementById(id)
     if (element.className.search("hovered") < 0) {
@@ -26,50 +23,6 @@ function gotoLeftRightUrl(k, urlLeft, urlRight) {
     } else if (urlRight != '' && k.keyCode == 39) {
         window.location = urlRight;
     }
-}
-
-function getCookie(c_name) {
-    var c_value = document.cookie;
-    var c_start = c_value.indexOf(" " + c_name + "=");
-    if (c_start == -1) {
-        c_start = c_value.indexOf(c_name + "=");
-    }
-    if (c_start == -1) {
-        c_value = null;
-    } else {
-        c_start = c_value.indexOf("=", c_start) + 1;
-        var c_end = c_value.indexOf(";", c_start);
-        if (c_end == -1) {
-            c_end = c_value.length;
-        }
-        c_value = unescape(c_value.substring(c_start, c_end));
-    }
-    return c_value;
-}
-
-function setCookie(c_name, value, exdays) {
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
-    document.cookie = c_name + "=" + c_value;
-}
-
-function displayCopyrightAlertBasedOnCookie() {
-//    var cookie = getCookie(COPYRIGHT_COOKIE_NAME)
-//    var copyrightAlert = document.getElementById('copyright-alert')
-//    var copyrightBlur = document.getElementById('copyright-blur')
-//    if (cookie == null || cookie != COPYRIGHT_COOKIE_VALUE) {
-//        copyrightAlert.style.visibility = "visible";
-//        copyrightBlur.style.visibility = "visible";
-//    } else {
-//        copyrightAlert.style.visibility = "hidden";
-//        copyrightBlur.style.visibility = "hidden";
-//    }
-}
-
-function acknowledgeCopyright() {
-    setCookie(COPYRIGHT_COOKIE_NAME, COPYRIGHT_COOKIE_VALUE, 365);
-    displayCopyrightAlertBasedOnCookie();
 }
 
 function detectBrowser() {
