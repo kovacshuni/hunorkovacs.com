25517b691128bd5d0cb9dd3c49dad184c2769a72
Hunor Kovács <kovacshuni@yahoo.com>
2014-01-17 12:51:21 +0200

    Resume added to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ HunorkovacsCom::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root :to => 'pages#home'
 
+  get 'resume' => 'pages#resume'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
