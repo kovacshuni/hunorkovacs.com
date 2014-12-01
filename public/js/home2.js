f4604a008884e050591f83ae4df3be77cf97e768
Hunor Kovács <kovacshuni@yahoo.com>
2014-12-01 15:57:08 +0200

    Still aligning. Footer ok.

--- public/js/home2.js
+++ public/js/home2.js
@@ -1,1 +1,13 @@
 $.backstretch("image/splash-original.jpg");
+
+var showingName = true;
+
+function toggleEmail() {
+    if (showingName) {
+        document.getElementById("fullname").innerHTML = "kovacshuni<wbr />@yahoo.com";
+        showingName = false;
+    } else {
+        document.getElementById("fullname").innerHTML = "Hunor Kovács";
+        showingName = true;
+    }
+}
