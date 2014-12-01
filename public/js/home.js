11e55893240f6ab05f36501ee7d12c53ff88a604
Hunor Kovács <kovacshuni@yahoo.com>
2014-12-01 18:20:44 +0200

    jeremypeters animated button

--- public/js/home.js
+++ public/js/home.js
@@ -19,3 +19,28 @@ function largen() {
 function smallen() {
     document.getElementById("theinput").style.width = "9.5em";
 }
+
+var app = angular.module('home', ['jp.ng-bs-animated-button']);
+
+app.controller('AskController', ['$scope', '$http', function($scope, $http) {
+    $scope.send = function() {
+        $scope.isSubmitting = true;
+        console.log($scope.answer);
+        $scope.isSubmitting = false;
+        $scope.result = 'success';
+        $scope.answer = null;
+    };
+
+    $scope.answer = null;
+
+    $scope.isSubmitting = null;
+    $scope.result = null;
+    $scope.options = {
+        onlyIcons: true,
+        buttonDefaultClass: 'btn-default btn-sm',
+        buttonInitialIcon: 'fa fa-angle-right',
+        buttonSuccessIcon: 'fa fa-check',
+        buttonSuccessClass: 'btn-default btn-sm',
+        animationCompleteTime: 500
+    };
+} ]);
