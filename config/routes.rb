57ff116697a7804cfbbb3b47e27b4206861c3cf7
Hunor Kovács <kovacshuni@yahoo.com>
2014-12-03 01:11:53 +0200

    Hide those answers.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'githelp' => 'pages#githelp'
 
   post 'answer' => 'answers#create'
-  get 'answers' => 'answers#index'
+  get 'CourageIsTheArtOfBeing' => 'answers#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
