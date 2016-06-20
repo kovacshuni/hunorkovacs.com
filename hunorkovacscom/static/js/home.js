471b61ab93151e4644947fa56f9525eec3178809
Hunor Kovacs <hunor.kovacs@ft.com>
2016-06-20 16:30:49 +0300

    New bg. Updated backstretch lib with align to bottom feature from github.com/apendua . Removed unnecessary akkastreams and angular and techincal.css files

--- hunorkovacscom/static/js/home.js
+++ hunorkovacscom/static/js/home.js
@@ -1,11 +1,11 @@
 
 function setBackStretch() {
     if (window.matchMedia('(max-width: 992px)').matches) {
-        $.backstretch("image/away-992x605.jpg");
+        $.backstretch("image/forest-992x744.jpg", {alignY: "bottom"});
     } else if (window.matchMedia('(max-width: 1200px)').matches) {
-        $.backstretch("image/away-1347x822.jpg");
+        $.backstretch("image/forest-1347x1010.jpg", {alignY: "bottom"});
     } else {
-        $.backstretch("image/away-original.jpg");
+        $.backstretch("image/forest-bw.jpg", {alignY: "bottom"});
     }
 }
 
@@ -22,41 +22,3 @@ function toggleEmail() {
         showingName = true;
     }
 }
-
-function stoppedTyping() {
-    if (this.value.length > 0) {
-        document.getElementById('answer_button').disabled = false;
-    } else {
-        document.getElementById('answer_button').disabled = true;
-    }
-}
-
-var app = angular.module('home', ['jp.ng-bs-animated-button']);
-
-app.controller('AskController', ['$scope', '$http', function($scope, $http) {
-    $scope.send = function() {
-        $scope.isSubmitting = true;
-        $http.post('/answer', $scope.answer, {headers: {'Content-Type': 'text/plain'}}).
-            success(function(data, status, headers, config) {
-                $scope.result = 'success';
-                $scope.answer = null;
-            }).
-            error(function(data, status, headers, config) {
-                $scope.result = 'error';
-                $scope.answer = null;
-            });
-    };
-
-    $scope.answer = null;
-
-    $scope.isSubmitting = null;
-    $scope.result = null;
-    $scope.options = {
-        onlyIcons: true,
-        buttonDefaultClass: 'btn-default btn-sm',
-        buttonInitialIcon: 'fa fa-angle-right',
-        buttonSuccessIcon: 'fa fa-check',
-        buttonSuccessClass: 'btn-default btn-sm',
-        animationCompleteTime: 500
-    };
-} ]);
