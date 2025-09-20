2d96f220d580b6dc9427fcd7b8447cddac5cee40
Hunor Kovacs <kovacshuni@gmail.com>
2025-09-20 23:09:22 +0200

    BMW X3 forsale webpage

--- hunorkovacscom.go
+++ hunorkovacscom.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "os"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    // Serve static files
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-    
-    // Serve specific HTML pages
-    http.HandleFunc("/", serveHome)
-    http.HandleFunc("/THEVASTATLANTIC", serveVastAtlantic)
-    http.HandleFunc("/HUNORIIKOVACSPFA", serveHunorPFA)
-    http.HandleFunc("/snow", serveSnow)
-    http.HandleFunc("/somethingspecial123", handler)
+	// Serve static files
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-    port := os.Getenv("PORT")
+	// Serve specific HTML pages
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/THEVASTATLANTIC", serveVastAtlantic)
+	http.HandleFunc("/HUNORIIKOVACSPFA", serveHunorPFA)
+	http.HandleFunc("/snow", serveSnow)
+	http.HandleFunc("/bmw", serveBMW)
+	http.HandleFunc("/somethingspecial123", handler)
+
+	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
@@ -28,25 +29,29 @@ func main() {
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        http.NotFound(w, r)
-        return
-    }
-    http.ServeFile(w, r, "index.html")
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	http.ServeFile(w, r, "index.html")
 }
 
 func serveVastAtlantic(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "thevastatlantic.html")
+	http.ServeFile(w, r, "thevastatlantic.html")
 }
 
 func serveHunorPFA(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "hunoriikovacspfa.html")
+	http.ServeFile(w, r, "hunoriikovacspfa.html")
 }
 
 func serveSnow(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "snow.html")
+	http.ServeFile(w, r, "snow.html")
+}
+
+func serveBMW(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "bmw-x3.html")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 }
